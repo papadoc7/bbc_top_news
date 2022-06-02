aecfd24cf35a5799ba86040c2f86e4b51d622daa
Georgios <georgios@simplycook.com>
2022-06-02 10:03:36 +0100

    adding webpack, index and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bbc_top_news
 Web app where you can see the top news in one of 5 pre listed countries
+
+
+### Development
+Checkout the project from the repo.
+
+```
+git clone https://github.com/papadoc7/bbc_top_news.git
+npm install
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }]
+  }
+}
